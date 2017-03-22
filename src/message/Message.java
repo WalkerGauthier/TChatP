f13fd2c37d68635b2cba8968ec60c58e4b023157
CompSci <CompSci@CHSRM105-04.bcos.org>
2017-03-22 07:13:24 -0500

    1st version TChatP

---
+++ src/message/Message.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+import message.gui.MainScreen;
+
+/**
+ *
+ * @author CompSci
+ */
+public class Message {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HomeScreen screen = new HomeScreen();
+        screen.show();
+        // TODO code application logic here
+    }
+    
+}
